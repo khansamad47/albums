28bfbe35fbf2f437c9d6773ed83c3358f4714248
Samad Khan <khan.samad47@gmail.com>
2017-08-10 22:04:33 -0400

    Added flexbox and centered header

--- index.android.js
+++ index.android.js
@@ -4,18 +4,26 @@
 import React from 'react'
 import { Text, AppRegistry } from 'react-native'
 
+// Import local stuff
+// * Need relative path and no .js is required
+import Header from './src/components/header';
+
 // Create App Component
+// This is the ROOT component
 const App = () => {
     // This is JSX: 
     // * Syntax like html
     // * Renders to Javascript
     // * babeljs.io
-    return (
-        <Text>SomeText</Text>
-    );
+    //return (
+    //    <Text>SomeText</Text>
+    //);
+
+    // Note that custom components are also invoked by JSX
+    return (<Header/>);
 };
 
 // Render to Device
 // * Here we are registering the main/parent component
-// * Only register one main component
+// * Only register the root component
 AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,54 @@
+// header.js 
+// Header Component will be defined here
+// Note that each file can have only one component
+// This is a child component
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// 1. Basic Component
+// Remember that component is a function which returns JSX
+// const Header = () => {
+//     const { textStyle } = styles;
+//     return <Text style={textStyle}>Albums</Text>;
+// };
+
+// 2. Component with a View
+// * View is used to contain components
+// * Position child on the screen using flexbox
+// * Flexbox is css based position technology
+// * It is used to position elements in a container 
+const Header = () => {
+    const { textStyle, viewStyle } = styles;
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums</Text>
+        </View>
+    );
+};
+
+// Styling
+// this is created as a dictionary
+// each sub component should have a sub dict
+const styles = {
+    textStyle: {
+        fontSize: 20
+    },
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center', // Flexbox vertical: 'flex-start', 'flex-end'
+        alignItems: 'center',     // Flexbox horizon: 'flex-start', 'flex-end'
+        height : 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: {width:0, height: 2},
+        shadowOpacity: 0.2,
+        elevation: 2,         // We dont know what these do yet
+        position: 'relative'  // We don't know what these do yet
+    }
+};
+
+// Need to export component always
+// Note that we only register the root component
+export default Header;
+
