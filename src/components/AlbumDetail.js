945934bab515546e95500172eb1915c0f9d3df8f
Samad Khan <khan.samad47@gmail.com>
2017-08-19 19:37:17 -0400

    Adding album art work

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -7,20 +7,27 @@ import CardSection from './CardSection';
 // destruvture props for effcialncy
 //const AlbumDetail = (props) => {
 const AlbumDetail = ({album}) => {
-    const {title, artist, thumbnail_image} = album;
+    const {title, artist, thumbnail_image, image} = album;
     return(
         <Card>
             <CardSection>
-                <View>
+                <View style = {styles.thumbnailContainerStyle} >
                     <Image 
                         style = {styles.thumbnailStyle}
                         source={{uri: thumbnail_image}}
                     />
                 </View>
                 <View style={styles.headerContentStyle}>
-                    <Text>{ title }</Text>
+                    <Text style = {styles.headerTextStyle}>
+                    { title }</Text>
                     <Text>{ artist }</Text>
                 </View>
+            </CardSection>
+            <CardSection>
+                <Image 
+                    style = {styles.imageStyle}
+                    source={{uri: image}}
+                />
             </CardSection>
         </Card>
     );
@@ -34,10 +41,25 @@ const styles = {
         justifyContent: 'space-around'
         // gives equal space around the items inside
     },
+    headerTextStyle:{
+        fontSize: 18,
+    },
     thumbnailStyle: {
         height: 50,
         width: 50,
-    }
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        width: 300,
+        flex: 1,
+        width: null, // This means it will take whole width
+    },
 }
 
 export default AlbumDetail;
